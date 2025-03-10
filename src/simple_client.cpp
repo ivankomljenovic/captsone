74a3b1013f58bb4e2f64d39ea3a97787040a1b81
Ivan Komljenovic <19iwrk@queensu.ca>
2025-03-10 16:56:28 -0400

    convert client code into class definition

--- src/simple_client.cpp
+++ src/simple_client.cpp
@@ -9,60 +9,89 @@
 
 using namespace std;
 
+class Client {
+    public:
+        int socketFD;
+        struct sockaddr_in serverAddress;
+        bool wasInitialized;
+        string addressName;
+        int portNumber;
+
+        Client(string serverIP, int portNumber){
+            addressName = serverIP;
+            this->portNumber = portNumber;
+
+            memset(&serverAddress, 0, sizeof(serverAddress));
+            serverAddress.sin_family = AF_INET;
+            serverAddress.sin_port = htons(portNumber);
+            inet_aton(serverIP.c_str(), &(serverAddress.sin_addr));
+
+            socketFD = 0;
+            wasInitialized = false;
+        }
+
+        int initSocket(){
+            if ((socketFD = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+                cout << "Error: Socket could not be initialized." << endl;
+                return -1;
+            }
+            
+            wasInitialized = true;
+
+            cout << "Socket successfully initialized." << endl;
+
+            return 0;
+        }
+
+        int closeSocket(){
+            if (!wasInitialized){
+                cout << "Error: Socket is not open." << endl;
+                return -1;
+            }
+            close(socketFD);
+
+            wasInitialized = false;
+            return 0;
+        }
+
+        int sendMessage(string message){
+            size_t bytesSent;
+
+            if (!wasInitialized){
+                cout << "Error: Socket was not initialized." << endl;
+                return -1;
+            }
+                
+
+            if ((bytesSent = sendto(socketFD, (const char *)message.c_str(), sizeof(message), 0, (const struct sockaddr *)&serverAddress, sizeof(serverAddress))) < 0){
+                cout << "Error: Could not send message." << endl;
+                return -1;
+            }
+
+            cout << "Message sent successfully:" << endl;        
+            cout << "Bytes sent: " << bytesSent << endl;
+            cout << "Address: " << addressName << endl;
+            cout << "Port: " << portNumber << endl;
+            cout << "Message: " << message << endl;
+
+            return 0;
+        }
+};
+
+
+
 int main(int argc, char *argv[]){
-
-    int my_socket;
     
-    if ((my_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        cout << "Error: Socket could not be created." << endl;
-        exit(1);
+    if (argc != 2){ // Requires 1 command line parameter, server IP address
+        cout << "Error: Server IP address requried. E.g. ./client 127.0.0.1" << endl;
     }
 
-    // Find server address
-    // string serv_name = "localhost";
-    struct sockaddr_in sa;
-    memset(&sa,0,sizeof(sa));
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(1864);
-    inet_aton("127.0.0.1", &(sa.sin_addr));
+    // Main program - send one test message
+    Client fpga_client(argv[1],1864);
 
-    // Bind not needed for client
+    fpga_client.initSocket();
 
-    // Send message to the server
-    char myMessage[12];
-    myMessage[0] = 'H';
-    myMessage[1] = 'E';
-    myMessage[2] = 'L';
-    myMessage[3] = 'L';
-    myMessage[4] = 'O';
-    myMessage[5] = ' ';
-    myMessage[6] = 'T';
-    myMessage[7] = 'H';
-    myMessage[8] = 'E';
-    myMessage[9] = 'R';
-    myMessage[10] = 'E';
-    myMessage[11] = '\0';
+    fpga_client.sendMessage("Hello World");
 
-    size_t byteCount;
-
-    if ((byteCount = sendto(my_socket,(const char *)myMessage,12,0,(const struct sockaddr *)&sa,sizeof(sa))) < 0) {
-        cout << "Error: Message not sent." << endl;
-    }
-
-    cout << "Sent " << byteCount << " bytes." << endl;
-    cout << "Address: " << inet_ntoa(sa.sin_addr) << endl;
-    cout << "Port: " << sa.sin_port << endl;
-    cout << "Zero: " << sa.sin_zero << endl;
-    cout << "Family: " << sa.sin_family << endl;
-
-    cout << "Waiting for reply" << endl;
-
-    if ((recvfrom(my_socket,myMessage,12,0,nullptr,nullptr)) < 0) {
-        cout << "Error: message receive error." << endl;
-    }
-    cout << "Reply received" << endl;
-    cout << myMessage << endl;
-
-    // Close the socket
-    close(my_socket);
+    fpga_client.closeSocket();
 }
