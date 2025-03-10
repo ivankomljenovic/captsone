f5d3a7410df3e91ee4d6854ce7651e6d01bd5947
Ivan Komljenovic <19iwrk@queensu.ca>
2025-03-10 18:54:31 -0400

    Write server code to continually receive messages

--- src/host_pc_server.cpp
+++ src/host_pc_server.cpp
@@ -12,74 +12,123 @@
 using namespace std;
 using namespace string_literals;
 
+class Server {
+    public:
+        int socketFD;
+        struct sockaddr_in serverAddress;
+        bool wasInitialized;
+        string addressName;
+        int portNumber;
+        bool verbose;
+        int bufferSize;
+
+        Server(string serverIP, int portNumber, int bufferSize, bool v){
+            addressName = serverIP;
+            this->portNumber = portNumber;
+            verbose = v;
+            this->bufferSize = bufferSize;
+
+            memset(&serverAddress, 0, sizeof(serverAddress));
+            serverAddress.sin_family = AF_INET;
+            serverAddress.sin_addr.s_addr = INADDR_ANY;
+            serverAddress.sin_port = htons(portNumber);
+            inet_aton(serverIP.c_str(), &(serverAddress.sin_addr));
+
+            socketFD = 0;
+            wasInitialized = false;
+        }
+
+        int initSocket(){
+            if ((socketFD = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+                cout << "Error: Socket could not be initialized." << endl;
+                return -1;
+            }
+
+            if ((::bind(socketFD, (const struct sockaddr *)&serverAddress, sizeof(serverAddress))) < 0){
+                cout << "Error: Could not bind socket." << endl;
+                return -1;
+            }
+            
+            wasInitialized = true;
+
+            cout << "Socket successfully initialized." << endl;
+
+            return 0;
+        }
+
+        int closeSocket(){
+            if (!wasInitialized){
+                cout << "Error: Socket is not open." << endl;
+                return -1;
+            }
+            close(socketFD);
+
+            wasInitialized = false;
+            return 0;
+        }
+
+        int sendMessage(string message){
+            size_t bytesSent;
+
+            if (!wasInitialized){
+                cout << "Error: Socket was not initialized." << endl;
+                return -1;
+            }
+                
+
+            if ((bytesSent = sendto(socketFD, (const char *)message.c_str(), sizeof(message), 0, (const struct sockaddr *)&serverAddress, sizeof(serverAddress))) < 0){
+                cout << "Error: Could not send message." << endl;
+                return -1;
+            }
+
+            if (verbose){
+                cout << "Message sent successfully:" << endl;        
+                cout << "Bytes sent: " << bytesSent << endl;
+                cout << "Address: " << addressName << endl;
+                cout << "Port: " << portNumber << endl;
+                cout << "Message: " << message << endl;
+            }
+
+            return 0;
+        }
+
+        int runServer (){
+            char message[bufferSize];
+            struct sockaddr sourceAddress;
+            socklen_t sourceLength;
+
+
+            while (true){
+                if ((recvfrom(socketFD, message, sizeof(message), 0, &sourceAddress, &sourceLength)) < 0){
+                    cout << "Error: Error receiving the message." << endl;
+
+                    cout << "Shutting down the server." << endl;
+                    return -1;
+                }
+
+                cout << "Received message: " << message << endl;
+            }
+        }
+};
+
 int main(int argc, char *argv[]){
+    bool verbose = false;
 
-    // Create a UDP socket in the IPv4 domain, using the IP protocol
-    int my_socket;
-
-    if ((my_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-        cout << "Error: Socket could not be created." << endl;
-        exit(1);
+    if (argc < 2){
+        cout << "Error: Requires the server IP address. E.g. ./server 127.0.0.1 -v" << endl;
+        return -1;
     }
 
-    // Bind the socket to the server's address
-    static struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    //inet_aton("127.0.0.1", &(server_address.sin_addr));
-    static unsigned short port = 1864;
-    server_address.sin_port = htons(port);
+    if (argc > 2 && (argv[2][0] == '-' && argv[2][1] == 'v'))
+        verbose = true;
 
-    inet_aton("127.0.0.1", &(server_address.sin_addr));
+    Server hostPC(argv[1], 1864, 10000, verbose);
 
-    // string ip_address = "130.15.85.123";
+    hostPC.initSocket();
 
-    // server_address.sin_addr.s_addr = inet_aton(ip_address.c_str(),&server_address.sin_addr);
-    // cout << "Server address: " << server_address.sin_addr.s_addr << endl;
+    cout << "Starting server." << endl;
 
-    if ((::bind(my_socket, (const struct sockaddr *)&server_address,sizeof(server_address))) < 0){
-        cout << "Error: Socket could not be bound with the following parameters:" << endl;
-        cout << "Family: " << server_address.sin_family << endl;
-        cout << "Port: " << server_address.sin_port << endl;
-        cout << "Zero: " << server_address.sin_zero << endl;
-        cout << "Address: " << server_address.sin_addr.s_addr << endl;
-        exit(1);
-    }
-    cout << "Socket bound with the following parameters:" << endl;
-    cout << "Family: " << server_address.sin_family << endl;
-    cout << "Port: " << server_address.sin_port << endl;
-    cout << "Zero: " << server_address.sin_zero << endl;
-    cout << "Address: " << server_address.sin_addr.s_addr << endl;
-    cout << "Server address converted: " << inet_ntoa(server_address.sin_addr) << endl;    
+    hostPC.runServer();
 
-    char incomingMessage[12];
-
-    struct sockaddr src_addr;
-    socklen_t src_len;
-
-    cout << "Waiting for message" << endl;
-    // Receive incoming message
-    if ((recvfrom(my_socket,incomingMessage,12,0,&src_addr,&src_len)) < 0){
-        cout << "Error: Message receive error." << endl;
-        exit(1);
-    }
-
-    cout << "Message received!" << endl;
-    cout << incomingMessage << endl;
-
-    // Send a reply back
-    incomingMessage[0] = 'R';
-    incomingMessage[1] = 'E';
-    incomingMessage[2] = 'P';
-    incomingMessage[3] = 'L';
-    incomingMessage[4] = 'Y';
-    incomingMessage[5] = '\0';
-
-    if ((sendto(my_socket, incomingMessage, 10, 0, &src_addr,sizeof(src_addr))) < 0){
-        cout << "Error: Error sending the reply." << endl;
-    }
-
-    // Close the socket
-    close(my_socket);
+    hostPC.closeSocket();
 }
