ab4bd05844792a71d6e9fb023f0712bd5945f750
Ivan Komljenovic <19iwrk@queensu.ca>
2025-04-04 12:30:20 -0400

    change server port number

--- src/host_pc_server.cpp
+++ src/host_pc_server.cpp
@@ -174,7 +174,7 @@ int main(int argc, char *argv[]){
 
 
     // Initialize the server
-    Server hostPC(argv[1], 5001, 2048, verbose);
+    Server hostPC(argv[1], 8080, 2048, verbose);
     hostPC.initSocket();
     cout << "Starting server." << endl;
     thread serverThread = hostPC.startServer();
